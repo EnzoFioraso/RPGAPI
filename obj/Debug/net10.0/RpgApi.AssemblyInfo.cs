e0a02c32f7877680230bea8dc48a095d8e9f8cec
EnzoFioraso <fioenzo3@gmail.com>
2026-09-16 22:29:32 -0300

    Aula08 New

--- obj/Debug/net10.0/RpgApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/RpgApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+229a10431562ae7bac70c017b40b65bdd207b8fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1f5f0269f0bfabd6c12303f8fa6a7f22886840")]
 [assembly: System.Reflection.AssemblyProductAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
